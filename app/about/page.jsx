d4ca1551aa5273bb521788e7107c7e42f56b247e
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-23 00:29:58 -0300

    created pages about & services

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+
+
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora maiores earum nam id, possimus fuga blanditiis. Hic, adipisci? Ducimus provident sint in odit et nobis aliquid accusamus voluptatem neque, dolorum, doloremque deserunt animi sed illum, reprehenderit assumenda dolore soluta cupiditate expedita facilis. Fugit officia tempora error perferendis harum? Soluta cupiditate explicabo rem corrupti possimus recusandae pariatur exercitationem. Deleniti, perferendis optio. Laborum excepturi similique eaque! Repudiandae nulla quae sint repellendus dolore consectetur minus rem facilis. Alias suscipit, consequatur earum non dignissimos cumque expedita quae, neque reiciendis aut, eveniet totam. Magnam rem mollitia minus dolor ipsa quidem unde doloremque laborum expedita iure.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,12 @@
+
+
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nostrum, nesciunt quisquam numquam omnis quas ab in commodi atque impedit quia ratione esse repudiandae vero, iure pariatur illo, similique rerum ea at. Sequi ducimus in provident deserunt ex, dolore, veniam atque, repellendus odio distinctio quaerat? Consectetur enim magni neque corporis!</p>
+    </div>
+  )
+}
+
+export default ServicesPage
