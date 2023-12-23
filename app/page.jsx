972c352463d294f5d236aa641630660a1f13a84e
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-23 01:24:01 -0300

    Rendering API data

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,17 @@ async function IndexPage() {
     <div>
         <h1>Index page</h1>
         <div>
-          {JSON.stringify(users)}
+         {
+          users.map((user) => (
+            <li key={user.id}>
+              <div>
+                <h5>{user.id} - {user.first_name} {user.last_name}</h5>
+                <p>{user.email}</p>
+              </div>
+              <img src={user.avatar} alt={user.first_name} />
+            </li>
+          ))
+         }
         </div>
     </div>
   )
