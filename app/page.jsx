94499b8d8e171ca3697a51d6803de4e5b80be7ef
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-23 01:15:37 -0300

    Using fetch function  for data users

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,18 @@
 
-function IndexPage() {
+async function fetchUsers() {//funcion
+ const res = await fetch('https://reqres.in/api/users')//pedir datos
+   const data = await res.json()//transformar datos
+   return data.data//devolver datos como arreglo de usuarios
+}
+
+async function IndexPage() {
+  const users = await fetchUsers()//ejecutar la funcion
   return (
     <div>
         <h1>Index page</h1>
+        <div>
+          {JSON.stringify(users)}
+        </div>
     </div>
   )
 }
